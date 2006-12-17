36fbb4bdf1e5aa58e1c077fca9f6994e00f4fce1
Neil Mitchell <ndmitchell@gmail.com>
2006-12-17 15:05:11 +0000

    Rewrite the Compos example, using the 3 styles of Play instance

--- Examples/ComposPaper.hs
+++ Examples/ComposPaper.hs
@@ -2,7 +2,13 @@
 
 module Examples.ComposPaper where
 
-import Data.PlayMPTC
+
+-- any one of the following will work
+import Examples.ComposBasic
+--import Examples.ComposOverlap
+--import Examples.ComposSYB
+
+
 import Data.List
 import Data.Char
 import Data.Maybe
@@ -10,22 +16,7 @@ import Control.Monad.State
 
 
 -- SECTION 2
-data Exp2 = EAbs2 String Exp2
-          | EApp2 Exp2 Exp2
-          | EVar2 String
-          deriving Show
-
 exp2_1 = EApp2 (EVar2 "a") (EAbs2 "b" (EVar2 "b"))
-
-
-
-instance Play Exp2 where
-    replaceChildren x =
-        case x of
-            EAbs2 a c -> playOne (EAbs2 a) c
-            EApp2 c1 c2 -> playTwo EApp2 c1 c2
-            x -> playDefault x
-
 
 
 rename2 :: Exp2 -> Exp2
@@ -63,22 +54,6 @@ fresh2 x = evalState (f [] x) names
 
 -- SECTION 4
 
-data Stm = SDecl Typ Var
-         | SAss  Var Exp
-         | SBlock [Stm]
-         | SReturn Exp
-         deriving Show
-
-data Exp = EStm Stm
-         | EAdd Exp Exp
-         | EVar Var
-         | EInt Int
-         deriving Show
-
-data Var = V String deriving Show
-
-data Typ = T_int | T_float deriving Show
-
 stm_1 = SBlock [SDecl T_int (V "x")
                ,SAss (V "x") (EInt 12)
                ,SAss (V "x") (EAdd (EVar (V "y")) (EAdd (EAdd (EInt 1) (EInt 2)) (EInt 3)))
@@ -86,95 +61,7 @@ stm_1 = SBlock [SDecl T_int (V "x")
                ]
 
 
--- THE BASIC PLAY INSTANCES
-
-instance Play Stm where
-    replaceChildren x =
-        case x of
-            SBlock x -> (x, SBlock)
-            SAss v x -> playMore (SAss v) x
-            SReturn x -> playMore SReturn x
-            x -> playDefault x
-
-
-instance Play Exp where
-    replaceChildren x =
-        case x of
-            EStm s -> playMore EStm s
-            EAdd a b -> playTwo EAdd a b
-            x -> playDefault x
-
-instance Play Var where
-    replaceChildren = playDefault
-
-
-{-
-PLAY INSTANCES RAW
-
-instance PlayEx Stm Stm where
-    replaceChildrenEx = playSelf
-
-
-instance PlayEx Stm Exp where
-    replaceChildrenEx x =
-        case x of
-            SAss x y -> playOne (SAss x) y
-            SReturn x -> playOne SReturn x
-            x -> playExDefault x
-
-instance PlayEx Exp Stm where
-    replaceChildrenEx x =
-        case x of
-            EStm x -> playOne EStm x
-            x -> playExDefault x
-
-
-instance PlayEx Stm Var where
-    replaceChildrenEx x =
-        case x of
-            SDecl typ var -> playOne (SDecl typ) var
-            SAss var e -> (var:collect, \(var:xs) -> SAss var (generate xs))
-                where (collect,generate) = replaceChildrenEx e
-            SReturn e -> playMore SReturn e
-            x -> playExDefault x
-
-
-instance PlayEx Exp Var where
-    replaceChildrenEx x =
-        case x of
-            EStm x -> playMore EStm x
-            EVar x -> playOne EVar x
-            x -> playExDefault x
--}
-
-
--- COMBINATOR BASED PLAY INSTANCES
-instance PlayEx Stm Stm where; replaceChildrenEx = playSelf
-instance PlayEx Exp Exp where; replaceChildrenEx = playSelf
-instance PlayEx Var Var where; replaceChildrenEx = playSelf
-
-instance Play a => PlayEx Exp a where
-    replaceChildrenEx x =
-        case x of
-            EStm a -> play EStm /\ a
-            EAdd a b -> play EAdd /\ a /\ b
-            EVar a -> play EVar /\ a
-            EInt a -> play EInt /\! a
-
-
-instance Play a => PlayEx Stm a where
-    replaceChildrenEx x =
-        case x of
-            SDecl a b -> play SDecl /\! a /\ b
-            SAss a b -> play SAss /\ a /\ b
-            SBlock a -> play SBlock /\ a
-            SReturn x -> play SReturn /\ x
-
-instance Play a => PlayEx Var a where
-    replaceChildrenEx = playDefault
-
-
--- MANIPULATIONS           
+-- MANIPULATIONS
 
 rename :: PlayEx x Var => x -> x
 rename = mapUnderEx $ \(V x) -> V ("_" ++ x)
