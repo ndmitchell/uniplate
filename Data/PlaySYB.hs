c1bcb464305bfe064b5595f849c65cb7746f1b4d
Neil Mitchell <ndmitchell@gmail.com>
2007-01-17 15:20:40 +0000

    Move to passing the remaining arguments around explicitly

--- Data/PlaySYB.hs
+++ Data/PlaySYB.hs
@@ -9,14 +9,14 @@ import Control.Monad.State
 
 
 instance (Data a, Typeable a) => Play a where
-    replaceChildren = collect_generate
+    replaceChildren x = fromCC (collect_generate x)
     
-    getChildren = concat . gmapQ getChildrenEx
+    getChildren x = concat (gmapQ getChildrenEx x)
     
 
 
 instance (Data a, Play b, Typeable a, Typeable b) => PlayEx a b where
-    replaceChildrenEx = collect_generate_self
+    replaceChildrenEx x = fromCC (collect_generate_self x)
 
     getChildrenEx x = res
         where
@@ -47,14 +47,18 @@ collect_generate item = (collect, generate)
 
 newtype C x a = C {fromC :: CC x a}
 
-type CC x a = ([x], [x] -> a)
+type CC x a = ([x], [x] -> (a, [x]))
+
+
+fromCC :: CC x a -> ([x], [x] -> a)
+fromCC (a, b) = (a, \i -> fst (b i))
 
 
 collect_generate_self :: (Data on, Play with, Typeable on, Typeable with) => on -> CC with on
 collect_generate_self x = res
         where
             res = case asTypeOf (cast x) (Just $ head $ fst res) of
-                       Just y -> ([y], \[x] -> fromJust (cast x))
+                       Just y -> ([y], \(x:xs) -> (fromJust (cast x), xs))
                        Nothing -> collect_generate x
 
 
@@ -62,8 +66,13 @@ collect_generate :: (Data on, Play with, Typeable on, Typeable with) => on -> CC
 collect_generate item = fromC $ gfoldl combine create item
     where
         -- forall a b . Data a => C with (a -> b) -> a -> C with b
-        combine (C (c,g)) x = C (c2 ++ c, \i -> let (a,b) = splitAt (length c2) i in g b (g2 a))
-            where (c2,g2) = collect_generate_self x
+        combine (C (c,g)) x = C (c2 ++ c, regen)
+            where
+                (c2,g2) = collect_generate_self x
+                
+                regen i = case g2 i of
+                            (x2,i2) -> case g i2 of
+                                (y2,i3) -> (y2 x2, i3)
         
         -- forall g . g -> C with g
-        create x = C ([], \[] -> x)
+        create x = C ([], \res -> (x, res))
