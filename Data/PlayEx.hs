92b3f9329b2f22eaaa7ed019ac3e609410d800f7
Neil Mitchell <ndmitchell@gmail.com>
2007-03-22 21:00:34 +0000

    Rewrite PlayEx in terms of PlayOn

--- Data/PlayEx.hs
+++ Data/PlayEx.hs
@@ -3,15 +3,20 @@
 module Data.PlayEx(module Data.Play, module Data.PlayEx) where
 
 import Data.Play
+import Data.PlayOn
 import Control.Monad
 
 
-class Play with => PlayEx on with where
-    replaceChildrenEx :: on -> ([with], [with] -> on)
-    
-    getChildrenEx :: on -> [with]
-    getChildrenEx = fst . replaceChildrenEx
+-- * The Class
 
+class Play to => PlayEx from to where
+    replaceType :: ReplaceType from to
+    
+    getType :: from -> [to]
+    getType = fst . replaceType
+
+
+-- * The Combinators
 
 playExDefault :: (Play on, PlayEx on with) => on -> ([with], [with] -> on)
 playExDefault x = (concat currents, generate . zipWith ($) generates . divide currents)
@@ -20,7 +25,7 @@ playExDefault x = (concat currents, generate . zipWith ($) generates . divide cu
         divide (x:xs) ys = y1 : divide xs y2
             where (y1,y2) = splitAt (length x) ys
     
-        (currents, generates) = unzip $ map replaceChildrenEx current
+        (currents, generates) = unzip $ map replaceType current
         (current, generate) = replaceChildren x
 
 
@@ -30,20 +35,7 @@ playSelf x = ([x], \[x] -> x)
 
 playMore :: PlayEx a b => (a -> c) -> a -> ([b],[b] -> c)
 playMore part item = (current, part . generate)
-    where (current, generate) = replaceChildrenEx item
-
-
-mapUnderEx :: PlayEx on with => (with -> with) -> on -> on
-mapUnderEx f x = generate $ map (mapUnder f) current
-    where (current, generate) = replaceChildrenEx x
-
-mapUnderExM :: (Monad m, PlayEx on with) => (with -> m with) -> on -> m on
-mapUnderExM f x = liftM generate $ mapM (mapUnderM f) current
-    where (current, generate) = replaceChildrenEx x
-
-
-allOverEx :: PlayEx on with => on -> [with]
-allOverEx = concatMap allOver . getChildrenEx
+    where (current, generate) = replaceType item
 
 
 play :: on -> ([with],[with] -> on)
@@ -54,7 +46,7 @@ play f = ([], \[] -> f)
 (/\) f item = (collect2,generate2)
     where
         (collectL,generateL) = f
-        (collectR,generateR) = replaceChildrenEx item
+        (collectR,generateR) = replaceType item
         collect2 = collectL ++ collectR
         generate2 xs = generateL a (generateR b)
             where (a,b) = splitAt (length collect2) xs
@@ -63,3 +55,28 @@ play f = ([], \[] -> f)
 (/\!) :: ([with], [with] -> item -> on) -> item -> ([with], [with] -> on)
 (/\!) (collect,generate) item = (collect,\xs -> generate xs item)
 
+
+-- * The Operations
+
+traverseEx :: PlayEx from to => (to -> to) -> from -> from
+traverseEx = traverseOn replaceType
+
+
+traverseExM :: (Monad m, PlayEx from to) => (to -> m to) -> from -> m from
+traverseExM = traverseOnM replaceType
+
+
+descendEx :: PlayEx from to => (to -> to) -> from -> from
+descendEx = descendOn replaceType
+
+
+descendExM :: (Monad m, PlayEx from to) => (to -> m to) -> from -> m from
+descendExM = descendOnM replaceType
+
+
+everythingEx :: PlayEx from to => from -> [to]
+everythingEx = concatMap everything . getType
+
+
+everythingContextEx :: PlayEx from to => from -> [(to, to -> from)]
+everythingContextEx = everythingContextOn replaceType
