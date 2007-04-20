54b7b38444274a3e7c8e73b06833e7064fbdd341
Neil Mitchell <ndmitchell@gmail.com>
2007-04-20 13:46:46 +0000

    Move classes and combinators out of the PlayEx and into PlayTypeable

--- Data/Generics/PlayEx.hs
+++ Data/Generics/PlayEx.hs
@@ -1,65 +1,22 @@
 {-# OPTIONS_GHC -fglasgow-exts #-}
 
-module Data.Generics.PlayEx(module Data.Generics.Play, module Data.Generics.PlayEx) where
+module Data.Generics.PlayEx(
+    module Data.Generics.PlayOn,
+    module Data.Generics.PlayEx
+    ) where
 
-import Data.Generics.Play
 import Data.Generics.PlayOn
-import Data.Typeable
-import Data.Maybe
-import Control.Monad
 
 
--- * The Classes
-
+-- * The Class
 
 -- | Children are defined as the top-most items of type to
 --   /starting at the root/.
-class (Typeable from, Typeable to, Play to) => PlayEx from to where
+class Play to => PlayEx from to where
     replaceType :: ReplaceType from to
     
     getType :: from -> [to]
     getType = fst . replaceType
-
-
--- | Children are defined as the top-most items of type to
---   /starting beneath the root/.
-class (Typeable from, Typeable to, Play to) => PlayAll from to where
-    replaceAll :: ReplaceType from to
-    
-    getAll :: from -> [to]
-    getAll = fst . replaceAll
-
-
-instance PlayAll a b => PlayEx a b where
-    replaceType x = res
-        where
-            res = case asTypeOf (cast x) (Just $ head $ fst res) of
-                      Nothing -> replaceAll x
-                      Just y -> ([y], \[y] -> fromJust $ cast y)
-
-
--- * The Combinators
-
-playSelf :: a -> ([a], [a] -> a)
-playSelf x = ([x], \[x] -> x)
-
-
-play :: on -> ([with],[with] -> on)
-play f = ([], \[] -> f)
-
-
-(|+) :: PlayEx item with => ([with], [with] -> item -> on) -> item -> ([with], [with] -> on)
-(|+) f item = (collect2,generate2)
-    where
-        (collectL,generateL) = f
-        (collectR,generateR) = replaceType item
-        collect2 = collectL ++ collectR
-        generate2 xs = generateL a (generateR b)
-            where (a,b) = splitAt (length collect2) xs
-
-
-(|-) :: ([with], [with] -> item -> on) -> item -> ([with], [with] -> on)
-(|-) (collect,generate) item = (collect,\xs -> generate xs item)
 
 
 -- * The Operations
--- Data/Generics/PlayTypeable.hs
+++ Data/Generics/PlayTypeable.hs
@@ -2,16 +2,56 @@
 
 -- this module should not be imported directly
 
-module Data.Generics.PlayTypeable(module Data.Generics.PlayEx, module Data.Generics.PlayTypeable) where
+module Data.Generics.PlayTypeable(
+    module Data.Generics.PlayEx,
+    module Data.Generics.PlayTypeable
+    ) where
 
 import Data.Generics.PlayEx
 import Data.Typeable
 import Data.Maybe
 
 
-instance (Typeable a, Typeable b, Play a, Play b, PlayAll a b) => PlayEx a b where
+instance (Typeable a, Typeable b, Play b, PlayAll a b) => PlayEx a b where
     replaceType x = res
         where
             res = case asTypeOf (cast x) (Just $ head $ fst res) of
                       Nothing -> replaceAll x
                       Just y -> ([y], \[y] -> fromJust $ cast y)
+
+
+
+
+-- | Children are defined as the top-most items of type to
+--   /starting beneath the root/.
+--
+--   This class should only be constructed with 'play', '|+' and '|-'
+class PlayAll from to where
+    replaceAll :: ReplaceType from to
+    
+    getAll :: from -> [to]
+    getAll = fst . replaceAll
+
+
+
+playSelf :: a -> ([a], [a] -> a)
+playSelf x = ([x], \[x] -> x)
+
+
+play :: on -> ([with],[with] -> on)
+play f = ([], \[] -> f)
+
+
+(|+) :: PlayEx item with => ([with], [with] -> item -> on) -> item -> ([with], [with] -> on)
+(|+) f item = (collect2,generate2)
+    where
+        (collectL,generateL) = f
+        (collectR,generateR) = replaceType item
+        collect2 = collectL ++ collectR
+        generate2 xs = generateL a (generateR b)
+            where (a,b) = splitAt (length collect2) xs
+
+
+(|-) :: ([with], [with] -> item -> on) -> item -> ([with], [with] -> on)
+(|-) (collect,generate) item = (collect,\xs -> generate xs item)
+
