401390e61668887416c2877e6c2624c91ed06ee9
Neil Mitchell <ndmitchell@gmail.com>
2007-04-21 22:04:27 +0000

    Add various par combinators to OperationsCommon

--- Benchmark/OperationsCommon.hs
+++ Benchmark/OperationsCommon.hs
@@ -7,13 +7,13 @@ import Data
 task :: Show a => String -> [(String,q -> a)] -> [(String,String,q -> String)]
 task x y = [(x,a,\i -> show (b i)) | (a,b) <- y]
 
+alt s (a,b) = (a ++ " " ++ s,b)
 
 
 rawExpr  f g = ("raw" , f . g . unwrapN)
 playExpr f g = ("play", f . g . unwrapN)
 sybExpr  f g = ("syb" , f . g . unwrapN)
 compExpr f g = ("comp", f . g . unwrapC)
-
 
 rawExpr2  = rawExpr  rewrapN
 playExpr2 = playExpr rewrapN
@@ -31,4 +31,14 @@ playStm2 = playStm rewrapStmN
 sybStm2  = sybStm  rewrapStmN
 compStm2 = compStm rewrapStmC
 
-alt s (a,b) = (a ++ " " ++ s,b)
+
+rawPar  f g = ("raw" , f . g . unwrapCN)
+playPar f g = ("play", f . g . unwrapCN)
+sybPar  f g = ("syb" , f . g . unwrapCN)
+compPar f g = ("comp", f . g . unwrapCC)
+
+rawPar2  = rawPar  rewrapCN
+playPar2 = playPar rewrapCN
+sybPar2  = sybPar  rewrapCN
+compPar2 = compPar rewrapCC
+
