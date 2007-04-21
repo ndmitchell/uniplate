d835216256e32e640a4916452e3c6b60024ea3cd
Neil Mitchell <ndmitchell@gmail.com>
2007-04-21 14:46:09 +0000

    Change the Main script to work with the new thing

--- Benchmark/Main.hs
+++ Benchmark/Main.hs
@@ -4,54 +4,43 @@ module Main where
 import System.Environment
 import System.CPUTime
 import Control.Monad
+import Data.List
+import qualified Data.Map as Map
 
 import Testset
 import Generate
 import Data
+import OperationsAll
 
-import qualified Compos
-import qualified Raw
-import qualified PlaySYB
-import qualified SYB
-import qualified Play
 
 
 main = getArgs >>= main2
 
-
 main2 [x] = case x of
     "gen" -> generate 100 >>= print
-    "t1" -> exec1 tasks1
-    "t2" -> exec1 tasks2
+    "expr" -> exec testset tasksExpr
 
 
 
-tasks1 = [("Compos", Compos.task1 . unwrapC)
-         ,("Raw", Raw.task1 . unwrapN)
-         ,("Play Ever", Play.task1_everything . unwrapN)
-         ,("Play Fold", Play.task1_fold . unwrapN)
-         ,("Slay Ever", PlaySYB.task1_everything . unwrapN)
-         ,("Slay Fold", PlaySYB.task1_fold . unwrapN)
-         ,("SYB", SYB.task1 . unwrapN)
-         ]
+groupOn f xs = groupBy ((==) `on` f) $ sortBy (compare `on` f) xs
+    where on g f x y = g (f x) (f y)
 
-tasks2 = [("Compos", rewrapC . Compos.task2 . unwrapC)
-         ,("Raw", rewrapN . Raw.task2 . unwrapN)
-         ,("Play Descend", rewrapN . Play.task2_descend   . unwrapN)
-         ,("Play Traverse", rewrapN . Play.task2_traverse  . unwrapN)
-         ,("Slay Descend", rewrapN . PlaySYB.task2_descend . unwrapN)
-         ,("Slay Traverse" , rewrapN . PlaySYB.task2_traverse . unwrapN)
-         ,("SYB"  , rewrapN . SYB.task2 . unwrapN)
-         ]
+fst3 (a,b,c) = a
 
-
-
-exec1 :: Eq a => [(String, Expr -> a)] -> IO ()
-exec1 tsks | ans == ans = mapM_ f tsks
+exec :: [a] -> [(String,String,a -> Res)] -> IO ()
+exec tsts ops = mapM_ (uncurry $ execTask tsts) (map f tasks)
     where
-        tests = concat $ replicate 100 testset
-    
-        ans = map (snd $ head tsks) tests
+        tasks = groupOn fst3 ops
+        f xs = (fst3 (head xs), [(b,c) | (a,b,c) <- xs])
+
+
+execTask :: [a] -> String -> [(String,a -> Res)] -> IO ()
+execTask tsts name ops | ans == ans = mapM_ f xs
+    where
+        ans = map (snd $ head ops) tests
+        tests = concat $ replicate 100 tsts
+        xs = Map.toList $ Map.fromList ops
+
         
         f (name, action) = do
             start <- getCPUTime
