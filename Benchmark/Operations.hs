0c65bc9945c2f18c913ba5ce1263f250a34c4619
Neil Mitchell <ndmitchell@gmail.com>
2007-04-21 13:45:02 +0000

    Add basic infrastructure for writing the operations

---
+++ Benchmark/Operations.hs
@@ -0,0 +1,46 @@
+-- This module requires tricky CPP'ing
+-- so that you can use 3 different Play instances
+
+module Operations where
+
+import Data
+import DeriveCompos
+import OperationsCommon
+import DeriveManual
+
+import Data.Generics.PlayEx as Play
+import Data.Generics as SYB
+
+
+tasksExpr = [variables]
+
+
+-- * SECTION 1
+
+
+variables = taskExpr "variables" [variables_raw, variables_play, variables_syb, variables_comp]
+
+
+variables_raw = rawStrings f
+    where
+        f (NVar  x    ) = [x]
+        f (NVal  x    ) = []
+        f (NNeg  x    ) = f x
+        f (NAdd  x y  ) = f x ++ f y
+        f (NSub  x y  ) = f x ++ f y
+        f (NMul  x y  ) = f x ++ f y
+        f (NDiv  x y  ) = f x ++ f y
+
+
+variables_play = playStrings $ \x -> [y | NVar y <- Play.everything x]
+
+variables_syb = sybStrings $ SYB.everything (++) ([] `mkQ` f)
+    where
+        f (NVar x) = [x]
+        f _ = []
+
+variables_comp = compStrings f
+    where
+        f :: GExpr a -> [String]
+        f (CVar x) = [x]
+        f x = composOpFold [] (++) f x
---
+++ Benchmark/OperationsCommon.hs
@@ -0,0 +1,24 @@
+
+module OperationsCommon where
+
+import Data
+
+
+taskExpr :: String -> [(String,Expr -> Res)] -> (String,[(String,Expr -> Res)])
+taskExpr = (,)
+
+
+data Res = RStrings [String]
+         | RExpr Expr
+
+
+rawWith  f g = ("raw" , f . g . unwrapN)
+playWith f g = ("play", f . g . unwrapN)
+sybWith  f g = ("syb" , f . g . unwrapN)
+compWith f g = ("comp", f . g . unwrapC)
+
+rawStrings  = rawWith  RStrings
+playStrings = playWith RStrings
+sybStrings  = sybWith  RStrings
+compStrings = compWith RStrings
+
