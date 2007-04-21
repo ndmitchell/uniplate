7d62c00ffd5cdaa0b96de95d60888070ca2b892d
Neil Mitchell <ndmitchell@gmail.com>
2007-04-21 17:10:43 +0000

    Change to a new scheme for operations defintion, add some more bits

--- Benchmark/Operations.hs
+++ Benchmark/Operations.hs
@@ -13,14 +13,15 @@ import Data.Generics as SYB
 
 
 tasksExpr = variables ++ zeros
+tasksStm = rename
 
 
 -- * SECTION 1
 
 
-variables = taskExpr "variables" [variables_raw, variables_play, variables_play2, variables_syb, variables_comp]
+variables = task "variables" [variables_raw, variables_play, variables_play2, variables_syb, variables_comp]
 
-variables_raw = rawStrings f
+variables_raw = rawExpr RStrings f
     where
         f (NVar  x    ) = [x]
         f (NVal  x    ) = []
@@ -31,19 +32,19 @@ variables_raw = rawStrings f
         f (NDiv  x y  ) = f x ++ f y
 
 
-variables_play = playStrings $ \x -> [y | NVar y <- Play.everything x]
+variables_play = playExpr RStrings $ \x -> [y | NVar y <- Play.everything x]
 
-variables_play2 = alt "fold" $ playStrings $ fold concat f
+variables_play2 = alt "fold" $ playExpr RStrings $ fold concat f
     where
         f (NVar x) c = x : c
         f _ c = c
 
-variables_syb = sybStrings $ SYB.everything (++) ([] `mkQ` f)
+variables_syb = sybExpr RStrings $ SYB.everything (++) ([] `mkQ` f)
     where
         f (NVar x) = [x]
         f _ = []
 
-variables_comp = compStrings f
+variables_comp = compExpr RStrings f
     where
         f :: GExpr a -> [String]
         f (CVar x) = [x]
@@ -51,9 +52,9 @@ variables_comp = compStrings f
 
 
 
-zeros = taskExpr "zeros" [zeros_raw, zeros_play, zeros_play2, zeros_syb, zeros_comp]
+zeros = task "zeros" [zeros_raw, zeros_play, zeros_play2, zeros_syb, zeros_comp]
 
-zeros_raw = rawInt f
+zeros_raw = rawExpr RInt f
     where
         f (NDiv  x (NVal 0)) = f x + 1
         f (NVar  x    ) = 0
@@ -64,19 +65,19 @@ zeros_raw = rawInt f
         f (NMul  x y  ) = f x + f y
         f (NDiv  x y  ) = f x + f y
 
-zeros_play = playInt $ \x -> length [() | NDiv _ (NVal 0) <- Play.everything x]
+zeros_play = playExpr RInt $ \x -> length [() | NDiv _ (NVal 0) <- Play.everything x]
 
-zeros_play2 = alt "fold" $ playInt $ fold sum f
+zeros_play2 = alt "fold" $ playExpr RInt $ fold sum f
     where
         f (NVar x) c = 1 + c
         f _ c = c
 
-zeros_syb = sybInt $ SYB.everything (+) (0 `mkQ` f)
+zeros_syb = sybExpr RInt $ SYB.everything (+) (0 `mkQ` f)
     where
         f (NDiv _ (NVal 0)) = 1
         f _ = 0
 
-zeros_comp = compInt f
+zeros_comp = compExpr RInt f
     where
         f :: GExpr a -> Int
         f (CDiv x (CVal 0)) = 1 + f x 
@@ -84,9 +85,9 @@ zeros_comp = compInt f
 
 
 
-simplify = taskExpr "simplify" [simplify_raw,simplify_play,simplify_play2,simplify_syb,simplify_compos]
+simplify = task "simplify" [simplify_raw,simplify_play,simplify_play2,simplify_syb,simplify_compos]
 
-simplify_raw = rawExpr f
+simplify_raw = rawExpr2 f
     where
         f (NSub x y) = NAdd (f x) (NNeg (f y))
         f (NAdd x y) = if x1 == y1 then NMul (NVal 2) x1 else NAdd x1 y1
@@ -100,20 +101,32 @@ simp (NSub x y)           = NAdd x (NNeg y)
 simp (NAdd x y) | x == y  = NMul (NVal 2) x
 simp x                    = x
 
-simplify_play = playExpr $ traverse simp
+simplify_play = playExpr2 $ traverse simp
 
-simplify_play2 = alt "rewrite" $ playExpr $ rewrite f
+simplify_play2 = alt "rewrite" $ playExpr2 $ rewrite f
     where
         f (NSub x y)           = Just $ NAdd x (NNeg y)
         f (NAdd x y) | x == y  = Just $ NMul (NVal 2) x
         f x                    = Nothing
 
-simplify_syb = sybExpr $ everywhere (mkT simp)
+simplify_syb = sybExpr2 $ everywhere (mkT simp)
 
-simplify_compos = compExpr f
+simplify_compos = compExpr2 f
     where
         f :: GExpr a -> GExpr a
         f (CSub x y) = CAdd (f x) (CNeg (f y))
         f (CAdd x y) = if x1 == y1 then CMul (CVal 2) x1 else CAdd x1 y1
             where (x1,y1) = (f x,f y)
         f x = composOp f x
+
+
+
+rename = task "rename" [rename_compos]
+
+
+rename_compos = compStm2 f
+    where
+        f :: CTree c -> CTree c
+        f t = case t of
+            CV x -> CV ("_" ++ x)
+            _ -> composOp f t
--- Benchmark/OperationsCommon.hs
+++ Benchmark/OperationsCommon.hs
@@ -4,35 +4,37 @@ module OperationsCommon where
 import Data
 
 
-taskExpr :: String -> [(String,Expr -> Res)] -> [(String,String,Expr -> Res)]
-taskExpr x y = [(x,a,b) | (a,b) <- y]
+task :: String -> [(String,q -> Res)] -> [(String,String,q -> Res)]
+task x y = [(x,a,b) | (a,b) <- y]
 
 
 data Res = RStrings [String]
          | RInt Int
          | RExpr Expr
+         | RStm Stm
          deriving (Show,Eq)
 
 
-rawWith  f g = ("raw" , f . g . unwrapN)
-playWith f g = ("play", f . g . unwrapN)
-sybWith  f g = ("syb" , f . g . unwrapN)
-compWith f g = ("comp", f . g . unwrapC)
+rawExpr  f g = ("raw" , f . g . unwrapN)
+playExpr f g = ("play", f . g . unwrapN)
+sybExpr  f g = ("syb" , f . g . unwrapN)
+compExpr f g = ("comp", f . g . unwrapC)
 
-rawStrings  = rawWith  RStrings
-playStrings = playWith RStrings
-sybStrings  = sybWith  RStrings
-compStrings = compWith RStrings
 
-rawInt  = rawWith  RInt
-playInt = playWith RInt
-sybInt  = sybWith  RInt
-compInt = compWith RInt
+rawExpr2  = rawExpr  (RExpr . rewrapN)
+playExpr2 = playExpr (RExpr . rewrapN)
+sybExpr2  = sybExpr  (RExpr . rewrapN)
+compExpr2 = compExpr (RExpr . rewrapC)
 
-rawExpr  = rawWith  (RExpr . rewrapN)
-playExpr = playWith (RExpr . rewrapN)
-sybExpr  = sybWith  (RExpr . rewrapN)
-compExpr = compWith (RExpr . rewrapC)
 
+rawStm  f g = ("raw" , f . g . unwrapStmN)
+playStm f g = ("play", f . g . unwrapStmN)
+sybStm  f g = ("syb" , f . g . unwrapStmN)
+compStm f g = ("comp", f . g . unwrapStmC)
+
+rawStm2  = rawStm  (RStm . rewrapStmN)
+playStm2 = playStm (RStm . rewrapStmN)
+sybStm2  = sybStm  (RStm . rewrapStmN)
+compStm2 = compStm (RStm . rewrapStmC)
 
 alt s (a,b) = (a ++ " " ++ s,b)
