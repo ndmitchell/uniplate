05f3c8ce8f61b6a9caf0ad7846c2eb66bf698e79
Neil Mitchell <ndmitchell@gmail.com>
2007-04-21 12:31:20 +0000

    Move to using the data type from my paper

--- Benchmark/Data.hs
+++ Benchmark/Data.hs
@@ -7,54 +7,81 @@ import Data.Generics
 
 -- * SECTION 1
 
-data Exp = EAbs String Exp
-         | EApp Exp Exp
-         | EVar String
-         deriving (Show, Read)
+data Expr  =  Val  Int         -- a literal value
+           |  Var  String      -- a variable
+           |  Neg  Expr        -- negation
+           |  Add  Expr  Expr  -- addition
+           |  Sub  Expr  Expr  -- subtraction
+           |  Mul  Expr  Expr  -- multiplication
+           |  Div  Expr  Expr  -- division
+           deriving (Eq,Show,Read)
 
 
 -- normal version
-data NExp = NAbs String NExp
-          | NApp NExp NExp
-          | NVar String
-          deriving (Typeable, Data)
+data NExpr  =  NVal  Int         -- a literal value
+            |  NVar  String      -- a variable
+            |  NNeg  NExpr        -- negation
+            |  NAdd  NExpr  NExpr  -- addition
+            |  NSub  NExpr  NExpr  -- subtraction
+            |  NMul  NExpr  NExpr  -- multiplication
+            |  NDiv  NExpr  NExpr  -- division
+            deriving (Data,Typeable)
 
 
-data DExp
+data DExpr
 
 -- Compos version
-type CExp = GExp DExp
+type CExpr = GExpr DExpr
 
 -- GADT version
-data GExp :: * -> * where
-    GAbs :: String -> CExp -> GExp DExp
-    GApp :: CExp -> CExp -> GExp DExp
-    GVar :: String -> GExp DExp
+data GExpr :: * -> * where
+    CVal :: Int           -> GExpr DExpr -- a literal value
+    CVar :: String        -> GExpr DExpr -- a variable
+    CNeg :: CExpr          -> GExpr DExpr -- negation
+    CAdd :: CExpr -> CExpr  -> GExpr DExpr -- addition
+    CSub :: CExpr -> CExpr  -> GExpr DExpr -- subtraction
+    CMul :: CExpr -> CExpr  -> GExpr DExpr -- multiplication
+    CDiv :: CExpr -> CExpr  -> GExpr DExpr -- division
 
 
 
-unwrapN :: Exp -> NExp
+unwrapN :: Expr -> NExpr
 unwrapN x = case x of
-    EAbs x y -> NAbs x (unwrapN y)
-    EApp x y -> NApp (unwrapN x) (unwrapN y)
-    EVar x -> NVar x
+    Val x -> NVal x
+    Var x -> NVar x
+    Neg x -> NNeg (unwrapN x)
+    Add x y -> NAdd (unwrapN x) (unwrapN y)
+    Sub x y -> NSub (unwrapN x) (unwrapN y)
+    Mul x y -> NMul (unwrapN x) (unwrapN y)
+    Div x y -> NDiv (unwrapN x) (unwrapN y)
 
-rewrapN :: NExp -> Exp
+rewrapN :: NExpr -> Expr
 rewrapN x = case x of
-    NAbs x y -> EAbs x (rewrapN y)
-    NApp x y -> EApp (rewrapN x) (rewrapN y)
-    NVar x -> EVar x
+    NVal x -> Val x
+    NVar x -> Var x
+    NNeg x -> Neg (rewrapN x)
+    NAdd x y -> Add (rewrapN x) (rewrapN y)
+    NSub x y -> Sub (rewrapN x) (rewrapN y)
+    NMul x y -> Mul (rewrapN x) (rewrapN y)
+    NDiv x y -> Div (rewrapN x) (rewrapN y)
 
 
-unwrapC :: Exp -> CExp
+unwrapC :: Expr -> CExpr
 unwrapC x = case x of
-    EAbs x y -> GAbs x (unwrapC y)
-    EApp x y -> GApp (unwrapC x) (unwrapC y)
-    EVar x -> GVar x
+    Val x -> CVal x
+    Var x -> CVar x
+    Neg x -> CNeg (unwrapC x)
+    Add x y -> CAdd (unwrapC x) (unwrapC y)
+    Sub x y -> CSub (unwrapC x) (unwrapC y)
+    Mul x y -> CMul (unwrapC x) (unwrapC y)
+    Div x y -> CDiv (unwrapC x) (unwrapC y)
 
-
-rewrapC :: CExp -> Exp
+rewrapC :: CExpr -> Expr
 rewrapC x = case x of
-    GAbs x y -> EAbs x (rewrapC y)
-    GApp x y -> EApp (rewrapC x) (rewrapC y)
-    GVar x -> EVar x
+    CVal x -> Val x
+    CVar x -> Var x
+    CNeg x -> Neg (rewrapC x)
+    CAdd x y -> Add (rewrapC x) (rewrapC y)
+    CSub x y -> Sub (rewrapC x) (rewrapC y)
+    CMul x y -> Mul (rewrapC x) (rewrapC y)
+    CDiv x y -> Div (rewrapC x) (rewrapC y)
