e7dfb697bb92a5b92b51f4523a99dd3917c0acc6
Neil Mitchell <ndmitchell@gmail.com>
2007-04-21 17:48:21 +0000

    Add Typeable Play instances for Stm

--- Benchmark/DeriveTypeable.hs
+++ Benchmark/DeriveTypeable.hs
@@ -20,3 +20,44 @@ instance (Typeable a, Play a) => PlayAll NExpr a where
             NSub a b  -> play NAdd |+ a |+ b
             NMul a b  -> play NAdd |+ a |+ b
             NDiv a b  -> play NAdd |+ a |+ b
+
+
+instance Play NStm where
+    replaceChildren = replaceChildrenAll
+
+instance Play NExp where
+    replaceChildren = replaceChildrenAll
+
+instance Play NVar where
+    replaceChildren = replaceChildrenAll
+
+instance Play NTyp where
+    replaceChildren = replaceChildrenAll
+
+
+instance (Typeable a, Play a) => PlayAll NStm a where
+    playAll x =
+        case x of
+            NSDecl a b -> play NSDecl |+ a |+ b
+            NSAss  a b -> play NSAss  |+ a |+ b
+            NSBlock a  -> play NSBlock |+ a
+            NSReturn a -> play NSReturn |+ a
+
+instance (Typeable a, Play a) => PlayAll NExp a where
+    playAll x =
+        case x of
+            NEStm a -> play NEStm |+ a
+            NEAdd a b -> play NEAdd |+ a |+ b
+            NEVar a  -> play NEVar |+ a
+            NEInt a -> play (NEInt a)
+
+instance (Typeable a, Play a) => PlayAll NVar a where
+    playAll x =
+        case x of
+            NV a -> play (NV a)
+
+instance (Typeable a, Play a) => PlayAll NTyp a where
+    playAll x =
+        case x of
+            NT_int -> play NT_int
+            NT_float -> play NT_float
