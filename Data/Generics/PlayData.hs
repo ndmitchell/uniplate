14982fb4888dd87c58f319e98d0ed748504621a5
Neil Mitchell <ndmitchell@gmail.com>
2007-04-23 10:35:49 +0000

    Move to using an oracle for the traversal (twice as fast!)

--- Data/Generics/PlayData.hs
+++ Data/Generics/PlayData.hs
@@ -70,7 +70,10 @@ contains x = if isAlgType dtyp then concatMap f ctrs else []
 
 
 instance (Data a, Typeable a) => Play a where
-    replaceChildren x = fromCC (collect_generate x)
+    replaceChildren = \x -> fromCC (collect_generate (fromBox answer) x)
+        where
+            answer :: Box a
+            answer = containsMatch (undefined :: a) (undefined :: a)
     
     getChildren = \x -> concatCont (gmapQ (getTypeOneWith (fromBox answer)) x) []
         where
@@ -80,7 +83,10 @@ instance (Data a, Typeable a) => Play a where
 
 
 instance (Data a, Data b, Play b, Typeable a, Typeable b) => PlayEx a b where
-    replaceType x = fromCC (collect_generate_self x)
+    replaceType = \x -> fromCC (collect_generate_self (fromBox answer) x)
+        where
+            answer :: Box b
+            answer = containsMatch (undefined :: a) (undefined :: b)
 
     getType = \x -> getTypeOneWith (fromBox answer) x []
         where
@@ -107,19 +113,22 @@ fromCC :: CC x a -> ([x], [x] -> a)
 fromCC (a, b) = (a [], \i -> fst (b i))
 
 
-collect_generate_self :: (Data on, Play with, Typeable on, Typeable with) => on -> CC with on
-collect_generate_self x = res
+collect_generate_self :: (Data on, Play with, Typeable on, Typeable with) =>
+                         (forall a . Typeable a => a -> Answer with) -> on -> CC with on
+collect_generate_self oracle x = res
         where
-            res = case asTypeOf (cast x) (Just $ head $ fst res []) of
-                       Just y -> ((y:), \(x:xs) -> (unsafeCast x, xs))
-                       Nothing -> collect_generate x
+            res = case oracle x of
+                       Hit y -> ((y:), \(x:xs) -> (unsafeCast x, xs))
+                       Follow -> collect_generate oracle x
+                       Miss -> (id, \res -> (x,res))
 
 
-collect_generate :: (Data on, Play with, Typeable on, Typeable with) => on -> CC with on
-collect_generate item = fromC $ gfoldl combine create item
+collect_generate :: (Data on, Play with, Typeable on, Typeable with) =>
+                    (forall a . Typeable a => a -> Answer with) -> on -> CC with on
+collect_generate oracle item = fromC $ gfoldl combine create item
     where
         -- forall a b . Data a => C with (a -> b) -> a -> C with b
-        combine (C (c,g)) x = case collect_generate_self x of
+        combine (C (c,g)) x = case collect_generate_self oracle x of
                                   (c2, g2) -> C (c2 . c, regen g2)
             where
                 regen g2 i = case g2 i of
