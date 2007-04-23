ab283a32548de05d4c75210d103be80544473e80
Neil Mitchell <ndmitchell@gmail.com>
2007-04-23 03:31:21 +0000

    Add container logic to PlayData - now outperforms SYB on queries

--- Data/Generics/PlayData.hs
+++ Data/Generics/PlayData.hs
@@ -8,21 +8,92 @@ import Data.Generics.PlayEx
 import Data.Generics.PlayInternal
 import Data.Generics
 import Data.Maybe
+import Data.List
+import qualified Data.IntSet as IntSet
 import Control.Monad.State
+import Debug.Trace
+
+
+
+-- | An existential box representing a type which supports SYB
+-- operations.
+data DataBox = forall a . (Typeable a, Data a) => DataBox a
+
+data Box find = Box {fromBox :: forall a . Typeable a => a -> Answer find}
+
+data Answer a = Hit {fromHit :: a} -- you just hit the element you were after (here is a cast)
+              | Follow -- go forward, you will find something
+              | Miss -- you failed to sink my battleship!
+
+
+containsMatch :: (Data start, Typeable start, Data find, Typeable find) =>
+                 start -> find ->
+                 Box find
+containsMatch start find = Box query
+    where
+        typeInt x = inlinePerformIO $ typeRepKey x
+    
+        query :: Typeable a => a -> Answer find
+        query a = if tifind == tia then Hit (unsafeCast a)
+                  else if tia `IntSet.member` timatch then Follow else Miss
+            where tia = typeInt $ typeOf a
+    
+        tifind = typeInt tfind
+        timatch = IntSet.fromList $ map typeInt tmatch
+
+        tfind = typeOf find
+        tmatch = f [tfind] (filter ((/=) tfind . fst) $ containsList start)
+
+        f want have = if null want2 then [] else want2 ++ f want2 no
+            where
+                want2 = map fst yes
+                (yes,no) = partition (not . null . intersect want . snd) have
+
+
+containsList :: (Data a, Typeable a) => a -> [(TypeRep, [TypeRep])]
+containsList x = f [] [DataBox x]
+    where
+        f done [] = []
+        f done (DataBox t:odo)
+            | tt `elem` done = f done odo
+            | otherwise = (tt,map (\(DataBox a) -> typeOf a) xs) : f (tt:done) (xs++odo)
+            where
+                tt = typeOf t
+                xs = contains t
+
+contains :: (Data a, Typeable a) => a -> [DataBox]
+contains x = if isAlgType dtyp then concatMap f ctrs else []
+    where
+        f ctr = gmapQ DataBox (asTypeOf (fromConstr ctr) x)
+        ctrs = dataTypeConstrs dtyp
+        dtyp = dataTypeOf x
 
 
 instance (Data a, Typeable a) => Play a where
     replaceChildren x = fromCC (collect_generate x)
     
-    getChildren x = concatCont (gmapQ getTypeOne x) []
+    getChildren = \x -> concatCont (gmapQ (getTypeOneWith (fromBox answer)) x) []
+        where
+            answer :: Box a
+            answer = containsMatch (undefined :: a) (undefined :: a)
     
 
 
-instance (Data a, Play b, Typeable a, Typeable b) => PlayEx a b where
+instance (Data a, Data b, Play b, Typeable a, Typeable b) => PlayEx a b where
     replaceType x = fromCC (collect_generate_self x)
 
-    getType x = getTypeOne x []
+    getType = \x -> getTypeOneWith (fromBox answer) x []
+        where
+            answer :: Box b
+            answer = containsMatch (undefined :: a) (undefined :: b)
 
+
+getTypeOneWith :: (Data a, Typeable a, Typeable b) =>
+                  (forall a . Typeable a => a -> Answer b) -> a -> [b] -> [b]
+getTypeOneWith f a b = case f a of
+                           Hit i -> i:b
+                           Follow -> concatCont (gmapQ (getTypeOneWith f) a) b
+                           Miss -> b
 
 
 getTypeOne :: (Data a, Typeable a, Typeable b) => a -> [b] -> [b]
