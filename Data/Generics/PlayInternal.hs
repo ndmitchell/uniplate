b95b2d07ed6756758673a687af4fa77c690e7e82
Neil Mitchell <ndmitchell@gmail.com>
2007-04-23 03:31:04 +0000

    Add inlinePerformIO to the Play internals

--- Data/Generics/PlayInternal.hs
+++ Data/Generics/PlayInternal.hs
@@ -1,20 +1,26 @@
 {-# OPTIONS_GHC -fglasgow-exts -cpp #-}
 
-module Data.Generics.PlayInternal(unsafeCast) where
+module Data.Generics.PlayInternal(unsafeCast, inlinePerformIO) where
 
 #ifdef __GLASGOW_HASKELL__
 
-import GHC.Exts(unsafeCoerce#)
+import GHC.Exts
+import Data.ByteString.Base
 
 unsafeCast :: a -> b
 unsafeCast = unsafeCoerce#
+
 
 #else
 
 import Data.Typeable
 import Data.Maybe
+import Foreign
 
 unsafeCast :: (Typeable a, Typeable b) => a -> b
 unsafeCast = fromJust . cast
+
+inlinePerformIO :: IO a -> a
+inlinePerformIO = unsafePerformIO
 
 #endif
