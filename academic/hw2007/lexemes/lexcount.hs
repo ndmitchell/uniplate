ede3a476c714f5f1ecec6def6e5993d4484d104a
Neil Mitchell <ndmitchell@gmail.com>
2007-06-03 17:02:29 +0000

    Update the lexeme counter to split by operation

--- academic/hw2007/lexemes/lexcount.hs
+++ academic/hw2007/lexemes/lexcount.hs
@@ -5,3 +5,14 @@ count file = readFile file >>= print . length . lexList
 lexList x = case lex x of
     [("",_)] -> []
     [(x,y)] -> x : lexList y
+
+count2 file = readFile file >>= putStrLn . unlines . map f . group . lines
+    where
+        group ("":xs) = group xs
+        group (x:"":xs) = x : group xs
+        group (x:y :xs) = group ((x ++ "\n" ++ y) : xs)
+        group x = x
+
+        f x | null lst = "(blank)"
+            | otherwise = head lst ++ " = " ++ show (length lst)
+            where lst = lexList x
