143b35850b0b8bc0086fb7ef24650efa104a42c4
Neil Mitchell <ndmitchell@gmail.com>
2007-06-03 15:29:42 +0000

    Various haddock improvements

--- Data/Generics/Uniplate.hs
+++ Data/Generics/Uniplate.hs
@@ -1,3 +1,15 @@
+{- |
+This is the main Uniplate module, which defines all the essential operations
+in a Haskell 98 compatible manner.
+
+Most functions have an example of a possible use for the function.
+To illustate, I have used the @Expr@ type as below:
+
+> data Expr = Val Int
+>           | Neg Expr
+>           | Add Expr Expr
+-}
+
 
 module Data.Generics.Uniplate where
 
@@ -8,47 +20,32 @@ import Data.Generics.PlateInternal
 
 -- * The Class
 
+-- | The type of replacing all the children of a node
+--
+--   Taking a value, the function should return all the immediate children
+--   of the same type, and a function to replace them.
 type ReplaceChildren on = on -> ([on], [on] -> on)
 
+-- | The standard Uniplate class, all operations require this
 class Uniplate on where
+    -- | The underlying method in the class
+    --
+    -- > replaceChildren (Add (Val 1) (Neg (Val 2))) = ([Val 1, Neg (Val 2)], \[a,b] -> Add a b)
+    -- > replaceChildren (Val 1)                     = ([]                  , \[]    -> Val 1  )
     replaceChildren :: ReplaceChildren on
     
 -- * The Operations
 
-children :: Uniplate on => on -> [on]
-children = fst . replaceChildren
+-- ** Queries
 
-
-transform :: Uniplate on => (on -> on) -> on -> on
-transform f x = f $ generate $ map (transform f) current
-    where (current, generate) = replaceChildren x
-
-
-transformM :: (Monad m, Uniplate on) => (on -> m on) -> on -> m on
-transformM f x = mapM (transformM f) current >>= f . generate
-    where (current, generate) = replaceChildren x
-
-
-rewrite :: Uniplate on => (on -> Maybe on) -> on -> on
-rewrite f = transform g
-    where g x = maybe x (rewrite f) (f x)
-
-
-rewriteM :: (Monad m, Uniplate on) => (on -> m (Maybe on)) -> on -> m on
-rewriteM f = transformM g
-    where g x = f x >>= maybe (return x) (rewriteM f)
-
-
-descend :: Uniplate on => (on -> on) -> on -> on
-descend f x = generate $ map f current
-    where (current, generate) = replaceChildren x
-
-    
-descendM :: (Monad m, Uniplate on) => (on -> m on) -> on -> m on
-descendM f x = liftM generate $ mapM f current
-    where (current, generate) = replaceChildren x
-
-
+-- | Get all the children of a node, including itself and all children.
+--
+-- > universe (Add (Val 1) (Neg (Val 2))) =
+-- >     [Add (Val 1) (Neg (Val 2)), Val 1, Neg (Val 2), Val 2]
+--
+-- This method is often combined with a list comprehension, for example:
+--
+-- > vals x = [Val i | i <- universe x]
 universe :: Uniplate on => on -> [on]
 universe x = builder (f x)
     where
@@ -56,6 +53,75 @@ universe x = builder (f x)
         f x cons nil = x `cons` concatCont (map (\x -> f x cons) $ children x) nil
 
 
+-- | Get the direct children of a node. Usually using 'universe' is more appropriate.
+--
+-- @children = fst . 'replaceChildren'@
+children :: Uniplate on => on -> [on]
+children = fst . replaceChildren
+
+
+
+-- ** Transformations
+
+
+-- | Transform every element in the tree, in a bottom-up manner.
+--
+-- For example, replacing negative literals with literals:
+--
+-- > negLits = trasform f
+-- >    where f (Neg (Lit i)) = Lit (negate i)
+-- >          f x = x
+transform :: Uniplate on => (on -> on) -> on -> on
+transform f x = f $ generate $ map (transform f) current
+    where (current, generate) = replaceChildren x
+
+
+-- | Monadic variant of 'transform'
+transformM :: (Monad m, Uniplate on) => (on -> m on) -> on -> m on
+transformM f x = mapM (transformM f) current >>= f . generate
+    where (current, generate) = replaceChildren x
+
+
+-- | Rewrite by applying a rule everywhere you can. Ensures that the rule cannot
+-- be applied anywhere in the result:
+--
+-- > propRewrite r x = all (isNothing . r) (universe (rewrite r x))
+--
+-- Usually 'transform' is more appropriate, but 'rewrite' can give better
+-- compositionality. Given two single transformations @f@ and @g@, you can
+-- construct @f `mplus` g@ which performs both rewrites until a fixed point.
+rewrite :: Uniplate on => (on -> Maybe on) -> on -> on
+rewrite f = transform g
+    where g x = maybe x (rewrite f) (f x)
+
+
+-- | Monadic variant of 'rewrite'
+rewriteM :: (Monad m, Uniplate on) => (on -> m (Maybe on)) -> on -> m on
+rewriteM f = transformM g
+    where g x = f x >>= maybe (return x) (rewriteM f)
+
+
+-- | Perform a transformation on all the immediate children, then combine them back.
+-- This operation allows additional information to be passed downwards, and can be
+-- used to provide a top-down transformation.
+descend :: Uniplate on => (on -> on) -> on -> on
+descend f x = generate $ map f current
+    where (current, generate) = replaceChildren x
+
+
+-- | Monadic variant of 'descend'    
+descendM :: (Monad m, Uniplate on) => (on -> m on) -> on -> m on
+descendM f x = liftM generate $ mapM f current
+    where (current, generate) = replaceChildren x
+
+
+
+-- ** Others
+
+-- | Return all the contexts and holes. This operation is only occasionally useful.
+--
+-- > propUniverse x = universe x == map fst (contexts x)
+-- > propId x = all (== x) [b a | (a,b) <- contexts x]
 contexts :: Uniplate on => on -> [(on, on -> on)]
 contexts x = (x,id) : f current
   where
@@ -64,7 +130,7 @@ contexts x = (x,id) : f current
            | (pre,b:post) <- zip (inits xs) (tails xs)
            , (y, context) <- contexts b]
 
-
+-- | Perform a fold on each value
 fold :: Uniplate on => (on -> [r] -> r) -> on -> r
 fold op x = op x $ map (fold op) $ children x
 
--- Data/Generics/UniplateOn.hs
+++ Data/Generics/UniplateOn.hs
@@ -1,3 +1,13 @@
+{- |
+This module retained Haskell 98 compatability, but users who are happy with
+multi-parameter type classes should look towards "Data.Generics.Biplate".
+
+The only function missing from "Data.Generics.Uniplate" is 'fold', as it can be
+constructed from 'children' and has little meaning in a multi-typed setting.
+
+All operations, apart from 'childrenOn' should perform identically to their non @On@
+counterparts.
+-}
 
 module Data.Generics.UniplateOn(
     module Data.Generics.Uniplate,
@@ -8,9 +18,30 @@ import Data.Generics.Uniplate
 import Control.Monad(liftM)
 import Data.List(inits,tails)
 
+-- * Types
 
+-- | Return all the top most children of type @to@ within @from@.
+--
+-- If @from == to@ then this function should return the root as the single
+-- child.
 type ReplaceType from to = from -> ([to], [to] -> from)
 
+
+-- * Operations
+
+-- ** Queries
+
+universeOn :: Uniplate to => ReplaceType from to -> from -> [to]
+universeOn replaceType x = concatMap universe $ fst $ replaceType x
+
+
+-- | Return the children of a type. If @to == from@ then it returns the
+-- original element (in constract to 'children'
+childrenOn :: Uniplate to => ReplaceType from to -> from -> [to]
+childrenOn replaceType x = fst $ replaceType x
+
+
+-- ** Transformations
 
 transformOn :: Uniplate to => ReplaceType from to -> (to -> to) -> from -> from
 transformOn replaceType f x = generate $ map (transform f) current
@@ -42,13 +73,7 @@ descendOnM replaceType f x = liftM generate $ mapM (descendM f) current
     where (current, generate) = replaceType x
 
 
-childrenOn :: Uniplate to => ReplaceType from to -> from -> [to]
-childrenOn replaceType x = fst $ replaceType x
-
-
-universeOn :: Uniplate to => ReplaceType from to -> from -> [to]
-universeOn replaceType x = concatMap universe $ fst $ replaceType x
-
+-- ** Other
 
 contextsOn :: Uniplate to => ReplaceType from to -> from -> [(to, to -> from)]
 contextsOn replaceType x =
@@ -58,6 +83,3 @@ contextsOn replaceType x =
         
         f pre x post = [(cur, \new -> generate (pre ++ [new] ++ post))
                        | (cur,gen) <- contexts x]
-
-
--- foldOn is not defined, does not have sensible semantics
