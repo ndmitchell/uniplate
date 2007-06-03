7e3148482b602bec16e31faa2a7bb4aa1c9da789
Neil Mitchell <ndmitchell@gmail.com>
2007-06-03 16:25:06 +0000

    Haddock changes

--- Data/Generics/PlateInternal.hs
+++ Data/Generics/PlateInternal.hs
@@ -1,6 +1,10 @@
 {-# OPTIONS_GHC -fglasgow-exts #-}
 {-# LANGUAGE CPP, Rank2Types #-}
 
+{- |
+    Internal module, do not import or use.
+-}
+
 module Data.Generics.PlateInternal(
     unsafeCast, inlinePerformIO, builder, concatCont
     ) where
@@ -18,14 +22,18 @@ import GHC.Exts(unsafeCoerce#, build, realWorld#)
 import GHC.IOBase(IO(IO))
 
 {-# INLINE unsafeCast #-}
+-- | @unsafeCoerce@, but for all compilers. In future this can be obtained from
+-- @Unsafe.Coerce.unsafeCoerce@, but thats too recent a change.
 unsafeCast :: a -> b
 unsafeCast = unsafeCoerce#
 
 {-# INLINE builder #-}
+-- | GHCs @foldr@\/@build@ system, but on all platforms
 builder :: forall a . (forall b . (a -> b -> b) -> b -> b) -> [a]
 builder = build
 
 {-# INLINE inlinePerformIO #-}
+-- | 'unsafePerformIO', but suitable for inlining. Copied from "Data.ByteString.Base".
 inlinePerformIO :: IO a -> a
 inlinePerformIO (IO m) = case m realWorld# of (# _, r #) -> r
 
@@ -64,6 +72,7 @@ builder f = f (:) []
 
 
 {-# INLINE concatCont #-}
+-- | Perform concatentation of continuations
 concatCont :: [a -> a] -> a -> a
 concatCont xs rest = foldr ($) rest xs
 
