0e044ba30566d5ace9debe0383c2f888616c25ca
Neil Mitchell <ndmitchell@gmail.com>
2007-12-20 12:03:13 +0000

    Move stuff out into Data.Generics.Str

---
+++ Data/Generics/Str.hs
@@ -0,0 +1,35 @@
+{- |
+This is the main central data structure in the Uniplate system.
+-}
+
+module Data.Generics.Str where
+
+import Data.Generics.PlateInternal
+
+
+-- * The Data Type
+
+data Str a = Zero | One a | Two (Str a) (Str a)
+             deriving Show
+
+
+strType :: Str a -> a
+strType = undefined
+
+strList x = builder (f x)
+    where
+        f (Two (One x) xs) cons nil = x `cons` f xs cons nil
+        f Zero cons nil = nil
+
+listStr (x:xs) = Two (One x) (listStr xs)
+listStr [] = Zero
+
+
+
+pam f Zero = Zero
+pam f (One x) = One (f x)
+pam f (Two x y) = Two (pam f x) (pam f y)
+
+pamM f Zero = return Zero
+pamM f (One x) = do x <- f x; return (One x)
+pamM f (Two x y) = do x <- pamM f x; y <- pamM f y; return (Two x y)
--- Data/Generics/Uniplate.hs
+++ Data/Generics/Uniplate.hs
@@ -16,34 +16,7 @@ module Data.Generics.Uniplate where
 import Control.Monad
 import Data.List(inits,tails)
 import Data.Generics.PlateInternal
-
-
--- * The Data Type
-
-data Str a = Zero | One a | Two (Str a) (Str a)
-             deriving Show
-
-
-strType :: Str a -> a
-strType = undefined
-
-strList x = builder (f x)
-    where
-        f (Two (One x) xs) cons nil = x `cons` f xs cons nil
-        f Zero cons nil = nil
-
-listStr (x:xs) = Two (One x) (listStr xs)
-listStr [] = Zero
-
-
-
-pam f Zero = Zero
-pam f (One x) = One (f x)
-pam f (Two x y) = Two (pam f x) (pam f y)
-
-pamM f Zero = return Zero
-pamM f (One x) = do x <- f x; return (One x)
-pamM f (Two x y) = do x <- pamM f x; y <- pamM f y; return (Two x y)
+import Data.Generics.Str
 
 
 -- * The Class
