ebde95e0a86bafc7f65d64dd7cae656de3030d85
Neil Mitchell <ndmitchell@gmail.com>
2008-07-01 12:13:41 +0000

    Add some white space and type signatures to Generics.Str

--- Data/Generics/Str.hs
+++ Data/Generics/Str.hs
@@ -16,28 +16,36 @@ import Data.Monoid
 data Str a = Zero | One a | Two (Str a) (Str a)
              deriving Show
 
+
 instance Functor Str where
   fmap f Zero = Zero
   fmap f (One x) = One (f x)
   fmap f (Two x y) = Two (fmap f x) (fmap f y)
+
 
 instance Foldable Str where
   foldMap m Zero = mempty
   foldMap m (One x) = m x
   foldMap m (Two l r) = foldMap m l `mappend` foldMap m r
 
+
 instance Traversable Str where
   traverse f Zero = pure Zero
   traverse f (One x) = One <$> f x
   traverse f (Two x y) = Two <$> traverse f x <*> traverse f y
 
+
 strType :: Str a -> a
 strType = undefined
 
+
+strList :: Str a -> [a]
 strList x = builder (f x)
     where
         f (Two (One x) xs) cons nil = x `cons` f xs cons nil
         f Zero cons nil = nil
 
+
+listStr :: [a] -> Str a
 listStr (x:xs) = Two (One x) (listStr xs)
 listStr [] = Zero
