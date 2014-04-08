dd8ec5d5efc9912b708018bf44cb423c2c5462e0
Neil Mitchell <ndmitchell@gmail.com>
2014-04-08 14:41:41 +0100

    Update travis to install haskell-src-exts for the tests

--- travis.hs
+++ travis.hs
@@ -3,6 +3,7 @@ import Neil
 
 main :: IO ()
 main = do
+    retry 3 $ cmd "cabal install haskell-src-exts"
     cmd "runhaskell Main test"
     cmd "ghc --make -O2 Main.hs -o uniplate"
     cmd "./uniplate benchmark"
