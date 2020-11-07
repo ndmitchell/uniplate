be1c7aa3f28abebe31a53cc975896455ff791a2c
Neil Mitchell <ndmitchell@gmail.com>
2020-11-07 18:52:21 +0000

    Disable the tests, since they don't work with cabal

--- travis.hs
+++ travis.hs
@@ -1,9 +1,11 @@
 
 import System.Process.Extra
+import Control.Monad
 import Control.Exception.Extra
 
 main :: IO ()
-main = do
+-- These tests no longer work on cabal3, since you can't install a package globally
+main = when False $ do
     retry 3 $ system_ "cabal install haskell-src-exts"
     system_ "runhaskell Main test"
     system_ "ghc --make -O2 Main.hs -o uniplate"
