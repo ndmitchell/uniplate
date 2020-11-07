90b7c360b9b08f855fbc4b30e18b7f48c2ed9f73
Neil Mitchell <ndmitchell@gmail.com>
2020-11-07 18:56:43 +0000

    Try and make Travis run properly with newer GHC

--- travis.hs
+++ travis.hs
@@ -1,12 +1,14 @@
 
 import System.Process.Extra
+import Data.List
 import Control.Monad
 import Control.Exception.Extra
 
 main :: IO ()
--- These tests no longer work on cabal3, since you can't install a package globally
-main = when False $ do
-    retry 3 $ system_ "cabal install haskell-src-exts"
+main = do
+    ver <- systemOutput_ "cabal --version"
+    let v3 = " 3." `isInfixOf` ver
+    retry 3 $ system_ $ "cabal install haskell-src-exts" ++ if v3 then " --lib" else ""
     system_ "runhaskell Main test"
     system_ "ghc --make -O2 Main.hs -o uniplate"
     system_ "./uniplate benchmark"
