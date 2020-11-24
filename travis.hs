27bb78be6817f5cf137b199dca09945d3783a1b0
Neil Mitchell <ndmitchell@gmail.com>
2020-11-24 11:11:43 +0000

    Restrict travis.hs to Linux

--- travis.hs
+++ travis.hs
@@ -3,9 +3,11 @@ import System.Process.Extra
 import Data.List
 import Control.Monad
 import Control.Exception.Extra
+import System.Info
 
 main :: IO ()
-main = do
+main = when (os == "linux") $ do
+    -- The script was only designed for Linux, so restrict it for now
     ver <- systemOutput_ "cabal --version"
     let v3 = " 3." `isInfixOf` ver
     retry 3 $ system_ $ "cabal install haskell-src-exts syb unordered-containers" ++ if v3 then " --lib" else ""
